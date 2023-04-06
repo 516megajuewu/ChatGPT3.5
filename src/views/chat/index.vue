33b67279c6af8a0bfd4b9ce1c7606de21df8a465
516megajuewu <120559805+516megajuewu@users.noreply.github.com>
2023-04-06 22:11:27 +0800

    更新 index.vue

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -2,7 +2,7 @@
 import { computed, onMounted, onUnmounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import { storeToRefs } from 'pinia'
-import { NAutoComplete, NButton, NIcon, NInput, NPopconfirm, useDialog, useMessage } from 'naive-ui'
+import { NAutoComplete, NButton, NInput, NPopconfirm, useDialog, useMessage } from 'naive-ui'
 import html2canvas from 'html2canvas'
 import { Message } from './components'
 import { useScroll } from './hooks/useScroll'
@@ -126,11 +126,14 @@ document.body.ontouchend = (event: any) => {
 }
 
 const startSpeechRecognition = (event: any) => {
+  let startTime = 135
   try {
     // if (isMobile.value && event.target.tagName !== 'IMG')
     //   return
-    if (event.target.tagName === 'IMG')
+    if (event.target.tagName === 'IMG') {
+      startTime = 1
       event.preventDefault()
+    }
   }
   catch (error) {}
   if (isRecording.value)
@@ -146,7 +149,7 @@ const startSpeechRecognition = (event: any) => {
   speechTimeoutId = setTimeout(() => {
     isRecording.value = true
     Voice.start()
-  }, 175)
+  }, startTime)
   return false
 }
 
@@ -662,17 +665,14 @@ onUnmounted(() => {
                 @keypress="handleEnter" @mousedown="startSpeechRecognition" @mouseup="stopSpeechRecognition"
               >
                 <template #suffix>
-                  <NIcon size="25" depth="1" :color="isRecording ? '#4b9e5f' : ''">
-                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24"><path d="M12 15c1.66 0 2.99-1.34 2.99-3L15 6c0-1.66-1.34-3-3-3S9 4.34 9 6v6c0 1.66 1.34 3 3 3zm-1.2-9.1c0-.66.54-1.2 1.2-1.2s1.2.54 1.2 1.2l-.01 6.2c0 .66-.53 1.2-1.19 1.2s-1.2-.54-1.2-1.2V5.9zm6.5 6.1c0 3-2.54 5.1-5.3 5.1S6.7 15 6.7 12H5c0 3.41 2.72 6.23 6 6.72V22h2v-3.28c3.28-.48 6-3.3 6-6.72h-1.7z" fill="currentColor" /></svg>
-                  </NIcon>
-                  <!-- <span class="text-xl" :class="{ 'text-[#4b9e5f]': isRecording }">
+                  <span class="text-xl" :class="{ 'text-[#4b9e5f]': isRecording, 'text-[#aaaaaa]': !isRecording }">
                     <SvgIcon icon="ic:outline-keyboard-voice" width="25" height="25" />
-                  </span> -->
+                  </span>
                 </template>
               </NInput>
             </template>
           </NAutoComplete>
-          <NButton type="primary" :disabled="buttonDisabled" @click="handleSubmit">
+          <NButton type="primary" :disabled="buttonDisabled">
             <template #icon>
               <span class="dark:text-black">
                 <SvgIcon icon="ri:send-plane-fill" />
