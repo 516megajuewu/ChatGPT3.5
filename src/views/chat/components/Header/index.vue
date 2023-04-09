bbc9df6cb5e942b4bc5edc895ded679c9870b009
516megajuewu <120559805+516megajuewu@users.noreply.github.com>
2023-04-09 17:19:17 +0800

    Update index.vue

--- src/views/chat/components/Header/index.vue
+++ src/views/chat/components/Header/index.vue
@@ -45,7 +45,7 @@ function toggleUsingContext() {
   <header
     class="sticky top-0 left-0 right-0 z-30 border-b dark:border-neutral-800 bg-white/80 dark:bg-black/20 backdrop-blur"
   >
-    <div class="relative flex items-center justify-between min-w-0 overflow-hidden h-14">
+    <div class="relative flex items-center justify-between min-w-0 overflow-hidden h-11">
       <div class="flex items-center">
         <button
           class="flex items-center justify-center w-11 h-11"
